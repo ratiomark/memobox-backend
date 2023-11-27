398202ca3eb5b9ac919bf60adce9bfb402919cc4
ratiomark <yanagae@gmail.com>
2023-11-27 03:35:07 +0200

    ref: update view page

--- src/user-data-storage/user-data-storage.service.ts
+++ src/user-data-storage/user-data-storage.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { Box, BoxSpecialType, MissedTrainingValue, User } from '@prisma/client';
-import { CommonShelfFrontedResponse } from '@/aggregate/entities/types';
+import {
+  Box,
+  BoxSpecialType,
+  Card,
+  MissedTrainingValue,
+  Shelf,
+  User,
+} from '@prisma/client';
+import {
+  CommonShelfFrontedResponse,
+  ShelfIncBoxes,
+  ShelfIncBoxesIncCards,
+} from '@/aggregate/entities/types';
 import { CardsService } from '@/cards/cards.service';
 import { commonShelfTemplate } from '@/common/const/commonShelfTemplate';
 import { NEW_CARDS_COUNTS_AS_TRAIN } from '@/common/const/flags';
-import {
-  ShelfFrontedResponse,
-  ShelvesCupboardFrontedResponse,
-} from '@/shelves/entities/shelf.entity';
+
 import { ShelvesService } from '@/shelves/shelves.service';
 import { CupboardSchema, BoxSchemaFrontend } from './types/fronted-responses';
 import { I18nService } from 'nestjs-i18n';
+import { cards } from 'prisma/mock-data/staticDataFromDb';
+import { timeLeft } from '@/cards/helpers/timeLeft';
+import { UpdateCardDto } from '@/cards/dto/update-card.dto';
+import { plainToClass } from 'class-transformer';
 // import { zonedTimeToUtc } from 'date-fns-tz';
 @Injectable()
 export class UserDataStorageService {
@@ -106,158 +118,62 @@ export class UserDataStorageService {
     return result;
   }
 
-  async getViewPageData(userId: User['id'], lang = 'en') {
+  async getViewPageData(userId: User['id']) {
     const [cards, shelvesAndBoxesData] = await Promise.all([
       this.cardsService.findAllWithBox(userId),
       this.shelvesService.getShelvesAndBoxesData(userId),
     ]);
 
-    const enhancedCardsPromises = cards.map(async (card) => {
-      let state = 'wait'; // Значение по умолчанию
+    const enhancedCards = cards.map((card) =>
+      this.cardsService.enhanceCard(card),
+    );
 
-      if (
-        card.box.specialType === BoxSpecialType.new &&
-        NEW_CARDS_COUNTS_AS_TRAIN
-      ) {
-        state = 'train';
-      } else if (card.nextTraining) {
-        // const nextTrainingDate = new Date(card.nextTraining);
-        // const now = new Date();
-
-        // Если nextTraining меньше текущего времени, то карточка в состоянии 'train'
-        // if (nextTrainingDate < now) {
-        if (card.nextTraining < new Date()) {
-          state = 'train';
-        }
-      }
-
-      const nextTraining = await timeLeft(card.nextTraining, this.i18n, lang);
-
-      return {
-        ...card,
-        lastTraining: card.lastTraining ? card.lastTraining : '---',
-        nextTraining,
-        isDeleting: false,
-        boxIndex: card.box.index,
-        specialType: card.box.specialType,
-        state,
-      };
-    });
-
-    const enhancedCards = await Promise.all(enhancedCardsPromises);
     return { cards: enhancedCards, shelvesAndBoxesData };
   }
-}
 
-async function timeLeft(
-  utcTime: Date | null,
-  i18nService: I18nService,
-  lang = 'en',
-) {
-  if (!utcTime) return 'пора';
-  // return await i18nService.translate('time.less_than_minute', { lang });
+  async getTrashPageData(userId: User['id'], lang = 'en') {
+    const deletedShelvesAndShelvesData =
+      await this.shelvesService.getDeletedShelves(userId);
 
-  const currentTime = new Date().getTime();
-  const diff = utcTime.getTime() - currentTime;
-  if (diff < 0) return 'пора';
-
-  // Конвертируем разницу в секунды, минуты, часы, дни, месяцы и годы
-  const diffSeconds = Math.floor(diff / 1000);
-  const diffMinutes = Math.floor(diffSeconds / 60);
-  const diffHours = Math.floor(diffMinutes / 60);
-  const diffDays = Math.floor(diffHours / 24);
-  const diffMonths = Math.floor(diffDays / 30); // Приблизительно
-  const diffYears = Math.floor(diffMonths / 12);
-
-  // Функция для интернационализации с учетом множественного числа
-  const i18n = async (number, unit) => {
-    return await i18nService.translate(
-      `common.time.${unit}${number > 1 ? '_plural' : ''}`,
-      { lang, args: { count: number } },
-    );
-  };
-
-  if (diffYears > 0) {
-    return await i18n(diffYears, 'year');
-  } else if (diffMonths > 0) {
-    return await i18n(diffMonths, 'month');
-  } else if (diffDays > 1) {
-    return await i18n(diffDays, 'day');
-  } else if (diffHours > 0) {
-    return `~ ${await i18n(diffHours, 'hour')}`;
-  } else if (diffMinutes > 0) {
-    return await i18n(diffMinutes, 'minute');
-  } else {
-    return await i18nService.translate('time.less_than_minute', { lang });
+    return getTrashPageDataFromDbData(deletedShelvesAndShelvesData);
   }
 }
-// function timeLeft(utcTime: Date | null, lang = 'en') {
-//   if (!utcTime) return 'пора';
-//   // const eventTime = utcTime.getTime();
-//   const currentTime = new Date().getTime();
-//   const diff = utcTime.getTime() - currentTime;
-//   if (diff < 0) return 'пора';
 
-//   // Конвертируем разницу в секунды, минуты, часы, дни, месяцы и годы
-//   const diffSeconds = Math.floor(diff / 1000);
-//   const diffMinutes = Math.floor(diffSeconds / 60);
-//   const diffHours = Math.floor(diffMinutes / 60);
-//   const diffDays = Math.floor(diffHours / 24);
-//   const diffMonths = Math.floor(diffDays / 30); // Приблизительно
-//   const diffYears = Math.floor(diffMonths / 12);
+function getTrashPageDataFromDbData(shelves: ShelfIncBoxesIncCards[]) {
+  if (shelves.length === 0) {
+    return {};
+  }
+  if (shelves.length === 1) {
+    const shelf = shelves[0];
+    return {
+      cards: shelf.card,
+      boxes: shelf.box,
+      shelves: [shelf],
+    };
+  }
+  // if (shelves.length === 3) {
+  //   const shelf = shelves[2];
+  //   return {
+  //     cards: shelf.card,
+  //     boxes: shelf.box,
+  //     shelves: [shelf],
+  //   };
+  // }
+  const boxes = shelves.reduce((acc, shelf) => {
+    const newBoxesObject = {
+      ...shelf.box,
+      shelfId: shelf.id,
+      shelfTitle: shelf.title,
+    };
+    return [newBoxesObject, ...acc];
+  }, []);
 
-//   // Функция для интернационализации
-//   const i18n = (number, unit) => {
-//     const units = {
-//       en: {
-//         day: 'day',
-//         month: 'month',
-//         year: 'year',
-//         hour: 'hour',
-//         minute: 'minute',
-//       },
-//       ru: { day: 'дн.', month: 'мес.', year: 'г.', hour: 'ч.', minute: 'мин.' },
-//     };
-//     return `${number} ${units[lang][unit]}`;
-//   };
-
-//   if (diffYears > 0) {
-//     return i18n(diffYears, 'year');
-//   } else if (diffMonths > 0) {
-//     return i18n(diffMonths, 'month');
-//   } else if (diffDays > 1) {
-//     return i18n(diffDays, 'day');
-//   } else if (diffHours > 0) {
-//     return `~ ${i18n(diffHours, 'hour')}`;
-//   } else if (diffMinutes > 0) {
-//     return i18n(diffMinutes, 'minute');
-//   } else {
-//     return lang === 'en' ? '< a minute' : '< минуты';
-//   }
-// }
-
-// export function timeLeft(utcTime: string | null) {
-//   if (!utcTime) return 'пора';
-//   // console.log(utcTime, '---------------');
-//   const eventTime = new Date(utcTime);
-//   // console.log(eventTime, '000000000000');
-//   const currentTime = new Date();
-//   // @ts-ignore
-//   const diff = eventTime - currentTime;
-//   if (diff < 0) return 'пора';
-//   // Конвертируем разницу в секунды, минуты, часы и дни
-//   const diffSeconds = Math.floor(diff / 1000);
-//   const diffMinutes = Math.floor(diffSeconds / 60);
-//   const diffHours = Math.floor(diffMinutes / 60);
-//   const diffDays = Math.floor(diffHours / 24);
-
-//   if (diffDays > 1) {
-//     return `${diffDays} дн.`;
-//   } else if (diffHours > 0) {
-//     return `${diffHours} ч.`;
-//   } else if (diffMinutes > 0) {
-//     return `${diffMinutes} мин.`;
-//   } else {
-//     return '< минуты';
-//   }
-// }
+  const cards = shelves.reduce((acc, shelf) => {
+    return [...shelf.card, ...acc];
+  }, []);
+  return {
+    shelves: shelves,
+    boxes,
+    cards,
+  };
+}
