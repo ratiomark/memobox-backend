f8f45bdf2e1842d8b6ae04488facc6d85258bcc9
ratiomark <yanagae@gmail.com>
2023-12-05 22:28:00 +0200

    @typescript-eslint/no-floating-promises ifnoreIIFE

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,12 @@ module.exports = {
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-floating-promises': [
+			'error',
+			{
+				ignoreIIFE: true
+			},
+		],
 		'no-unused-vars': 'off',
 		'require-await': 'off',
 		'@typescript-eslint/no-unused-vars': ['warn'],
