91c820519c65a478c202984461b8254aac7b5754
ratiomark <yanagae@gmail.com>
2023-12-15 16:31:30 +0200

    ref: sequential execution of tests

---
+++ test/cards/cards.test.ts
@@ -0,0 +1,5 @@
+import cardsRetrieveTests from './01-cards-training-retrieve.case';
+import cardsTrainingResponsesTests from './02-cards-training-responses.case';
+
+cardsRetrieveTests();
+cardsTrainingResponsesTests();
