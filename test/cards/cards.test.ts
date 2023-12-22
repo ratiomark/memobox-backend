7af6a2280d3e95cbc495640d0fa2e58be8f9748b
ratiomark <yanagae@gmail.com>
2023-12-23 01:27:25 +0200

    deploy VPS, check test flow

--- test/cards/cards.test.ts
+++ test/cards/cards.test.ts
@@ -1,5 +1,8 @@
+import { APP_URL } from 'test/utils/constants';
 import cardsRetrieveTests from './01-cards-training-retrieve.case';
 import cardsTrainingResponsesTests from './02-cards-training-responses.case';
-
 cardsRetrieveTests();
 cardsTrainingResponsesTests();
+console.log('cards.test.ts');
+console.log(process.env.NODE_ENV);
+console.log(APP_URL);
