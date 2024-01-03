da81a37a9a7dee0c853331964832244b87f5dbe4
ratiomark <yanagae@gmail.com>
2024-01-03 22:52:45 +0200

    ad: lock keys

--- src/common/const/lock-keys-patterns.ts
+++ src/common/const/lock-keys-patterns.ts
@@ -1,4 +1,6 @@
 export const LOCK_KEYS = {
   updateCardsAfterTraining: 'updateCardsAfterTraining',
+  creatingNewShelf: 'creatingNewShelf',
+  removingShelfToTrash: 'removingShelfToTrash',
 } as const;
 export type LockKeys = keyof typeof LOCK_KEYS;
