5429f5c503c6a49b320cd8c83035329c277a6668
ratiomark <yanagae@gmail.com>
2024-01-05 22:24:20 +0200

    Add shelf events

--- src/common/const/events.ts
+++ src/common/const/events.ts
@@ -1,2 +1,4 @@
 export const EVENT_USER_CREATED = 'user.created';
+export const EVENT_SHELF_CREATED = 'shelf.created';
+export const EVENT_SHELF_DELETED = 'shelf.deletedToTrash';
 export const EVENT_NOTIFY_EMAIL = 'notify.email';
