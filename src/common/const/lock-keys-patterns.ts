b79fbec19c9b87bd95e039a9cb7791b65a090e04
ratiomark <yanagae@gmail.com>
2024-01-10 18:33:07 +0200

    Add new lock key for removing box from shelf to trash

--- src/common/const/lock-keys-patterns.ts
+++ src/common/const/lock-keys-patterns.ts
@@ -2,5 +2,6 @@ export const LOCK_KEYS = {
   updateCardsAfterTraining: 'updateCardsAfterTraining',
   creatingNewShelf: 'creatingNewShelf',
   removingShelfToTrash: 'removingShelfToTrash',
+  removingBoxFromShelfToTrash: 'removingBoxFromShelfToTrash',
 } as const;
 export type LockKeys = keyof typeof LOCK_KEYS;
