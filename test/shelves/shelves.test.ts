aeffee339a135dd25eed4feff28ba858e3904930
ratiomark <yanagae@gmail.com>
2024-01-12 16:20:00 +0200

    Add shelves update order test case

--- test/shelves/shelves.test.ts
+++ test/shelves/shelves.test.ts
@@ -1,4 +1,6 @@
 import shelvesMainActions from './01-shelves-main-actions.case';
+import shelvesUpdateOrder from './02-shelves-update-order.case';
 // import cardsTrainingResponsesTests from './02-cards-training-responses.case';
 shelvesMainActions();
+shelvesUpdateOrder();
 // cardsTrainingResponsesTests();
