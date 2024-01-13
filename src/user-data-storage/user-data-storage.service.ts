60da1e40eebdba9e6a5d6beee07e5e31388ad14e
ratiomark <yanagae@gmail.com>
2024-01-13 15:51:07 +0200

    update docker exec command

--- src/user-data-storage/user-data-storage.service.ts
+++ src/user-data-storage/user-data-storage.service.ts
@@ -290,7 +290,7 @@ export class UserDataStorageService implements OnModuleInit {
       }
       const dumpCommand = isDevelopment
         ? `${postgresBinPath}\\pg_dump -U ${username} --clean ${dbName} > ${postgresBinPath}\\db_backup.dump`
-        : `docker exec ${dbHost} pg_dump -U ${username} -h ${dbHost} --clean ${dbName} > /backups/db_backup.dump`;
+        : `docker exec ${dbHost} sh -c "pg_dump -U ${username} -h ${dbHost} --clean ${dbName} > /backups/db_backup.dump"`;
       // : `pg_dump -U ${username} -h ${dbHost} --clean ${dbName} > /tmp/db_backup.dump`;
 
       const { stdout } = await execAsync(dumpCommand, {
@@ -335,7 +335,7 @@ export class UserDataStorageService implements OnModuleInit {
       }
       const restoreCommand = isDevelopment
         ? `${postgresBinPath}\\psql -U ${username} -d ${dbName} -f ${postgresBinPath}\\db_backup.dump`
-        : `docker exec ${dbHost} psql -U ${username} -d ${dbName} -h ${dbHost} -f /backups/db_backup.dump`;
+        : `docker exec ${dbHost} sh -c "psql -U ${username} -d ${dbName} < /backups/db_backup.dump"`;
 
       const { stdout } = await execAsync(restoreCommand, {
         env: {
