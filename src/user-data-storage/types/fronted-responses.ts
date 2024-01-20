4ffae79f6945b00f3cb404409a33e07840b4b920
ratiomark <yanagae@gmail.com>
2024-01-20 05:16:25 +0200

    ref: fronted trash response

--- src/user-data-storage/types/fronted-responses.ts
+++ src/user-data-storage/types/fronted-responses.ts
@@ -1,7 +1,8 @@
-import { Box } from '@prisma/client';
+import { Box, BoxSpecialType, Card, Shelf } from '@prisma/client';
 import { CommonShelfFrontedResponse } from '@/aggregate/entities/types';
-import { DataBlock } from '@/common/types/frontend/types';
+import { DataBlock, TimingBlock } from '@/common/types/frontend/types';
 import { ShelvesCupboardFrontedResponse } from '@/shelves/entities/shelf.entity';
+import { JsonValue } from '@prisma/client/runtime/library';
 
 export interface BoxSchemaFrontend extends Partial<Box> {
   // export interface BoxSchemaFrontend extends Omit<Box, 'userId' | 'shelfId'> {
@@ -11,4 +12,21 @@ export interface BoxSchemaFrontend extends Partial<Box> {
 export interface CupboardSchema {
   commonShelf: CommonShelfFrontedResponse;
   shelves: ShelvesCupboardFrontedResponse[];
+}
+
+export interface ShelvesTrashResponse extends Shelf {
+  box: Array<{
+    card: Card[];
+    _count: { card: number };
+    specialType: BoxSpecialType;
+    index: number;
+    deletedAt: Date | null;
+    timing: JsonValue;
+  }>;
+  _count: {
+    card: number;
+    box: number;
+  };
+  boxesCount: number;
+  cardsCount: number;
 }
