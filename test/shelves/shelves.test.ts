be3bcfe4d0160f69d98ca9b5f253d6143560bd52
ratiomark <yanagae@gmail.com>
2024-01-20 05:18:08 +0200

    Add shelves delete and restore test case

--- test/shelves/shelves.test.ts
+++ test/shelves/shelves.test.ts
@@ -1,6 +1,7 @@
 import shelvesMainActions from './01-shelves-main-actions.case';
 import shelvesUpdateOrder from './02-shelves-update-order.case';
-// import cardsTrainingResponsesTests from './02-cards-training-responses.case';
+import shelvesDeleteRestore from './03-shelves-delete-restore.case';
+
 shelvesMainActions();
 shelvesUpdateOrder();
-// cardsTrainingResponsesTests();
+shelvesDeleteRestore();
