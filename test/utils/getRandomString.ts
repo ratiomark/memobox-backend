df75061f351da4415823607184a5e77dacebfe90
ratiomark <yanagae@gmail.com>
2024-01-20 05:17:58 +0200

    Add getRandomString utility function

---
+++ test/utils/getRandomString.ts
@@ -0,0 +1,11 @@
+export const generateRandomString = (length = 10) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
