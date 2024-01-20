4ed46032d31cc844eb6cceb97a480df1f55f8b5f
ratiomark <yanagae@gmail.com>
2024-01-20 23:18:08 +0200

    add: EVENT_SHELF_RESTORED

--- src/common/const/events.ts
+++ src/common/const/events.ts
@@ -2,6 +2,7 @@ export const EVENT_USER_CREATED = 'user.created';
 export const EVENT_SHELF_CREATED = 'shelf.created';
 export const EVENT_SHELF_ORDER_CHANGED = 'shelf.orderChanged';
 export const EVENT_SHELF_DELETED = 'shelf.deletedToTrash';
+export const EVENT_SHELF_RESTORED = 'shelf.restoredFromTrash';
 export const EVENT_BOX_DELETED = 'box.deletedToTrash';
 export const EVENT_BOX_RESTORED = 'box.restoredFromTrash';
 export const EVENT_SHELF_BOXES_UPDATE = 'shelf.boxes.updated';
