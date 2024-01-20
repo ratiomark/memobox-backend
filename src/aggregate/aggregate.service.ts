80d4b92bbd75f40fa451a267e1343558fbf054c0
ratiomark <yanagae@gmail.com>
2024-01-20 23:15:55 +0200

    add: getDbTimeAndTimezone

--- src/aggregate/aggregate.service.ts
+++ src/aggregate/aggregate.service.ts
@@ -1,71 +1,13 @@
-// import { Injectable } from '@nestjs/common';
-// import { CreateAggregateDto } from './dto/create-aggregate.dto';
-// import { UpdateAggregateDto } from './dto/update-aggregate.dto';
-// import { CardsService } from '@/cards/cards.service';
-// import { ShelvesService } from '@/shelves/shelves.service';
-// import { User } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
 
-// @Injectable()
-// export class AggregateService {
-//   constructor(
-//     private readonly cardsService: CardsService,
-//     private readonly shelvesService: ShelvesService,
-//   ) {}
-//   // create(createAggregateDto: CreateAggregateDto) {
-//   //   return 'This action adds a new aggregate';
-//   // }
-//   // async getViewPageData(userId: User['id']) {
-//   //   const [cards, shelvesAndBoxesData] = await Promise.all([
-//   //     this.cardsService.findAllWithBox(userId),
-//   //     this.shelvesService.getShelvesAndBoxesData(userId),
-//   //   ]);
-//   //   const enhancedCards = cards.map((card) => {
-//   //     let state = 'wait'; // Значение по умолчанию
+@Injectable()
+export class AggregateService {
+  constructor(private readonly prisma: PrismaService) {}
 
-//   //     if (card.nextTraining) {
-//   //       const nextTrainingDate = new Date(card.nextTraining);
-//   //       const now = new Date();
-
-//   //       // Если nextTraining больше текущего времени, то карточка в состоянии 'train'
-//   //       if (nextTrainingDate > now) {
-//   //         state = 'train';
-//   //       }
-//   //     }
-
-//   //     const boxIndex = card.box.index;
-//   //     let specialType = 'none';
-//   //     switch (boxIndex) {
-//   //       case 0:
-//   //         specialType = 'new';
-//   //         break;
-//   //       case shelvesAndBoxesData[card.shelfId].maxIndexBox:
-//   //         specialType = 'learnt';
-//   //         break;
-//   //     }
-
-//   //     return {
-//   //       ...card,
-//   //       boxIndex,
-//   //       specialType,
-//   //       state,
-//   //     };
-//   //   });
-//   //   return { cards: enhancedCards, shelvesAndBoxesData };
-//   // }
-
-//   // findAll() {
-//   //   return `This action returns all aggregate`;
-//   // }
-
-//   // findOne(id: number) {
-//   //   return `This action returns a #${id} aggregate`;
-//   // }
-
-//   // update(id: number, updateAggregateDto: UpdateAggregateDto) {
-//   //   return `This action updates a #${id} aggregate`;
-//   // }
-
-//   // remove(id: number) {
-//   //   return `This action removes a #${id} aggregate`;
-//   // }
-// }
+  async getDbTimeAndTimezone() {
+    return await this.prisma.$queryRawUnsafe(
+      `SELECT NOW() as current_time, (SELECT setting FROM pg_settings WHERE name = 'TimeZone') as timezone;`,
+    );
+  }
+}
