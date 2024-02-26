f68ba2642a68167a3a6c2dffe8aa600de2b2e8c0
ratiomark <yanagae@gmail.com>
2024-02-26 19:25:06 +0200

    add lambda

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MailService } from './mail.service';
 import { MailerModule } from '@/mailer/mailer.module';
+import { AwsModule } from '@/aws/aws.module';
 
 @Module({
-  imports: [ConfigModule, MailerModule],
+  imports: [ConfigModule, AwsModule, MailerModule],
   providers: [MailService],
   exports: [MailService],
 })
